f7728bbba841dc4455ef7a922fbd08517de264dc
popovaludmila <57451404+popovaludmila@users.noreply.github.com>
2020-03-05 17:44:14 +0300

    начало

---
+++ js/backend.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function () {
+  var URL = 'https://js.dump.academy/code-and-magick';
+
+  window.save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      onLoad(xhr.response);
+    });
+
+    xhr.open('POST', URL);
+    xhr.send(data);
+  };
+
+
+})();
